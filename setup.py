59ad8b4fa86bfdc0a03073156fc625f3505514e8
Yigit Ozen <canyigitozen@gmail.com>
2014-06-09 18:24:11 +0300

    Small Fixes

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-#! /usr/bin/env python
+# ! /usr/bin/env python
+
 
 def setup_package():
     build_requires = []
@@ -7,34 +8,34 @@ def setup_package():
     except:
         build_requires = ['numpy>=1.5.1']
 
-   	metadata = dict(
-		name = 'ColorFinder',
-		version = '0.1',
-		description = 'A tool for finding colors in an image',
-		author = 'Yigit Ozen',
-		packages = ['colorfinder',],
-		package_data = {
-			'colorfinder': ['*.json'],
-		},
-		data_files = [("", ["LICENSE"])],
-		license = 'MIT',
-		setup_requires = build_requires,
-		install_requires = [
-			'Pillow',
-			'numpy',
-			'scipy',
-			'six >= 1.3',
-			'scikit-image',
-			'scikit-learn',
-		],
- 	)
+    metadata = dict(
+        name='ColorFinder',
+        version='0.1',
+        description='A tool for finding colors in an image',
+        author='Yigit Ozen',
+        packages=['colorfinder', ],
+        package_data={
+            'colorfinder': ['*.json'],
+        },
+        data_files=[("", ["LICENSE", "README"])],
+        license='MIT',
+        setup_requires=build_requires,
+        install_requires=[
+            'Pillow',
+            'numpy',
+            'scipy',
+            'six >= 1.3',
+            'scikit-image',
+            'scikit-learn',
+        ],
+    )
 
-	try:
-		from setuptools import setup
-	except ImportError:
-		from distutils.core import setup
+    try:
+        from setuptools import setup
+    except ImportError:
+        from distutils.core import setup
 
-	setup(**metadata)
+    setup(**metadata)
 
 
 if __name__ == '__main__':
